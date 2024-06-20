173371f38abe878a5e613b07c3782f8992b8c4d4
chizu praise <chizupraise31@gmail.com>
2024-06-20 06:56:19 -0700

    modified navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../App';
-import { auth } from '../firebaseConfig';
-import { signOut } from 'firebase/auth';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../App";
+import { auth } from "../firebaseConfig";
+import { signOut } from "firebase/auth";
 
 function Navbar() {
   const { user, setUser } = useContext(UserContext);
 
   const getInitials = (name) => {
-    if (!name) return '';
-    const nameParts = name.split(' ');
-    const initials = nameParts.map(part => part[0]).join('');
+    if (!name) return "";
+    const nameParts = name.split(" ");
+    const initials = nameParts.map((part) => part[0]).join("");
     return initials.toUpperCase();
   };
 
@@ -18,14 +18,14 @@ function Navbar() {
     try {
       await signOut(auth);
       setUser(null);
-      localStorage.removeItem('user');
+      localStorage.removeItem("user");
     } catch (error) {
-      console.error('Error signing out:', error);
+      console.error("Error signing out:", error);
     }
   };
 
   return (
-    <div className="bg-white md:p-4 flex flex-col items-center dark:bg-zinc-900 sticky top-0 z-50 shadow-xl">
+    <div className="bg-zinc-100 md:p-4 flex flex-col items-center dark:bg-zinc-900 sticky top-0 z-50 shadow-xl md:shadow-none">
       <header className="w-full max-w-3xl flex justify-between items-center p-2 bg-white dark:bg-zinc-800 rounded sm:p-4  ">
         <div className="text-2xl font-bold">
           <Link to="/" className="text-black dark:text-white">
@@ -33,13 +33,22 @@ function Navbar() {
           </Link>
         </div>
         <nav className="flex space-x-4 md:block hidden">
-          <Link to="/" className="text-black dark:text-white underline hover:text-green-700 decoration-solid">
+          <Link
+            to="/"
+            className="text-black dark:text-white underline hover:text-green-700 decoration-solid"
+          >
             Home
           </Link>
-          <Link to="/confess" className="text-black dark:text-white hover:underline hover:text-blue-900">
+          <Link
+            to="/confess"
+            className="text-black dark:text-white hover:underline hover:text-blue-900"
+          >
             Confess
           </Link>
-          <Link to="/confessions" className="text-black dark:text-white hover:underline hover:text-blue-900">
+          <Link
+            to="/confessions"
+            className="text-black dark:text-white hover:underline hover:text-blue-900"
+          >
             Confessions
           </Link>
         </nav>
